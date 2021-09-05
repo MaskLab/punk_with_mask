65c5d10499323e045f070a62e994e4d96acc0f8a
punkmask <punkmask@masklab.com>
2021-09-05 19:51:00 +0800

    add gen.sh

---
+++ gen.sh
@@ -0,0 +1,6 @@
+python ./script/mask.py --input=./img/punks.png --output=./punk_mask_img/tron --chain="TRON" > ./punk_mask_img/tron/gen_info
+
+python ./script/mask.py --input=./img/punks.png --output=./punk_mask_img/ethereum --chain="Ethereum" > ./punk_mask_img/ethereum/gen_info
+
+python ./script/mask.py --input=./img/punks.png --output=./punk_mask_img/bsc --chain="BSC" > ./punk_mask_img/bsc/gen_info
+
